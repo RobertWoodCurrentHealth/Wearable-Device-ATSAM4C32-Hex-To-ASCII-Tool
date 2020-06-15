d43a7a4deeb970789a84144a22b14f55f33929ad
Robert Wood <rob.wood@currenthealth.com>
2020-06-15 13:24:37 +0100

    Updated the Python script with comments and added CRC line check of Intel hex file

--- SAM4_HexToText.py
+++ SAM4_HexToText.py
@@ -1,6 +1,21 @@
 import sys
 
-# = /* CRC lookup table [B]polynomial 0xA001[/B] */
+## @package Hex to Text file
+# Reads in an Intel hex file and parses it, a line at a time 
+# then turns that Intel hex file into a text file suitable
+# for the SAM4 to program itself by downloading this newfile
+# over HTTPS.
+# The program constructs a large array the holds every value of the
+# SAM's flash memory. It is initalised with every value holding 0xFFFF
+# This means the program knows if a page has been filled with anything
+# as at least one of the page's values will be a none 0xFFFF value as 
+# it will have been overwritten with a eight bit value read from the 
+# Intel hex file
+# NOTE: Although the memory starts from 0x1000000 in the SAM, the array 
+# obviously starts at 0, so 0x1000000 is taken off the memory address
+# read in the Intel hex file and added back when writing to the new file
+
+# = /* CRC lookup table */
 crc16tab =  [
     0x0000, 0xC0C1, 0xC181, 0x0140, 0xC301, 0x03C0, 0x0280, 0xC241,
     0xC601, 0x06C0, 0x0780, 0xC741, 0x0500, 0xC5C1, 0xC481, 0x0440,
@@ -41,38 +56,61 @@ BASE_ADDRESS = 0x1000000
 SAM4C_PAGE_SIZE = 512
 ETX = 0x3
 
+##
+# @fn get_crc(data_array,starting_point,number_of_bytes,starting_value)
+# @brief
+# Creates a 16-bit CRC from integers
+# @param[in] data_array - the array of data from which to take the data used to create the CRC
+# @param[in] starting_point - the first value in the array from where to take the data to create the CRC
+# @param[in] number_of_bytes - how many bytes to use to create the CRC
+# @param[in] starting_value - The initial value of the CRC
 def get_crc(data_array,starting_point,number_of_bytes,starting_value):
-    ret_val = starting_value
-    #print("Starting value is " + hex(starting_point) + " number of bytes is " + hex(number_of_bytes))
+    ret_val = starting_value    
     for i in range(starting_point, starting_point + number_of_bytes):
         latestValue = (data_array[i] & 0xFF)
         index = ((ret_val ^ latestValue) & 0xff);
         ret_val = crc16tab[index] ^ ((ret_val >> 8) & 0xFF)
     return ret_val
-        
+
+##
+# @fn get_crc_from_string(istr,start_value):
+# @brief
+# Creates a 16-bit CRC from a string by reading 16-bit values from the string
+# one byte at a time
+# @return Returns the CRC
+# @param[in] istr - the input string from which to create the CTC
+# @param[in] start_value - The first position in the string from which to start
+# reading the data
 def get_crc_from_string(istr,start_value):
     ret_val = start_value
-    slen = len(istr)
-    #print("The incoming string is " + istr)
+    slen = len(istr)    
     for i in range(0,slen):    
-        value = ord(istr[i])
-        #print("value is " + hex(value))
+        value = ord(istr[i])        
         if value != 0x20 and value != 0x0A and value != 0x0D:
             aval = value   
             index = ((ret_val ^ (aval & 0xFF)) & 0xff)
-            ret_val = (crc16tab[index] ^ ((ret_val >> 8) & 0xFF))
-            #print("Index is " + hex(index) + " crc lookup is " + hex(crc16tab[index]))
+            ret_val = (crc16tab[index] ^ ((ret_val >> 8) & 0xFF))            
     return ret_val
         
         
     
     
-
+##
+# @fn create_page(address,nfw,marr)
+# @brief
+# Creates a string which is written to the target file. 
+# The sting is a page of data to be written to the SAM which starts with
+# a * character, then the start address of where the data is to be written
+# then the number of bytes to be written, the data that is to be written. 
+# Finally a CRC is calculated and written to the end of the text to be written 
+# to the file. All these values are separated by spaces to make the file easy to read.
+# @param[in] address - the address in flash to where the data is to be written 
+# @param[in] nfw - the file to be written to
+# @param[in] marr - The prefilled array of data from which to create the page
 def create_page(address,nfw,marr):
     line_index = 0
     line_string = ""
-    page_string = ""
-    #print("Writing a page at address ", hex(BASE_ADDRESS + address))
+    page_string = ""    
     page_string = "* " + format(BASE_ADDRESS + address,'x').zfill(7) + " " + format(SAM4C_PAGE_SIZE,'x').zfill(3) + "\r\n"   
     
     for a in range(0,SAM4C_PAGE_SIZE):
@@ -80,8 +118,7 @@ def create_page(address,nfw,marr):
         new_value = (marr[address + a] & 0xFF)
         new_string = format(new_value,'x').zfill(2)
         line_string += new_string
-        if line_index == 16:
-            #nfw.write(line_string + "\r\n")
+        if line_index == 16:            
             page_string += line_string + "\r\n"
             line_index = 0
             line_string = ""
@@ -90,27 +127,28 @@ def create_page(address,nfw,marr):
     if line_index != 0:
         page_string += line_string
     
-    crc = get_crc_from_string(page_string,0xFFFF)
-    #print("The CRC is " + hex(crc))
-    #nfw.write(format((crc >> 8) & 0xFF,'x').zfill(2) + " " + format(crc  & 0xFF,'x').zfill(2) + " " + chr(ETX) + "\r\n")
+    crc = get_crc_from_string(page_string,0xFFFF)   
     page_string += format((crc >> 8) & 0xFF,'x').zfill(2) + " " + format(crc  & 0xFF,'x').zfill(2) + " " + chr(ETX) + "\r\n"
-     
-     
+          
     nfw.write(page_string)
     
     
     
-    
-def check_page_bondary(address,marr):
-    #print("Check page boundary starting at " + hex(address))
+##
+# @fn check_page_boundary(address,marr):   
+# @brief
+# Check the array to ensure that there is some data to be written
+# return True or False
+# @param[in] address - the start address of the page of data to check for something to write
+# @param[in] marr - The array of data
+def check_page_boundary(address,marr):   
     if address % SAM4C_PAGE_SIZE == 0:
         ret_val = True
     else:
         ret_val = False
         
     if ret_val:
-        dataInPage = False;
-        #print("Check from address " + hex(address) + " to " + hex(address + SAM4C_PAGE_SIZE))
+        dataInPage = False;        
         for a in range(address, address + SAM4C_PAGE_SIZE):
             if marr[a] != 0xFFFF:
                 dataInPage = True                
@@ -120,13 +158,19 @@ def check_page_bondary(address,marr):
     return ret_val
 
 
-###########################################################
+##
+# @fn write_new_file(new_file,memory_array,end_address):
+# @brief
+# Write a file suitable for the SAM to read and self program
+# @param[in] new_file - the new file to write to
+# @param[in] memory_array - The prefilled array of SAM data, taken from the Intel hex file
+# @param[in] end_address - The final address within the array from which to take data
 def write_new_file(new_file,memory_array,end_address):
     first_page = True
     highest_address = 0
     create_erase_section(new_file,0,memory_array)
     for addr in range(0, MEMORY_ARRAY_SIZE):
-        if check_page_bondary(addr,memory_array):
+        if check_page_boundary(addr,memory_array):
             if first_page:                
                 lowest_address = addr                
                 first_page = False
@@ -141,7 +185,14 @@ def write_new_file(new_file,memory_array,end_address):
     new_file.close()
 ###########################################################
 
-###########################################################
+
+##
+# @fn detect_data_in_page(start_addr,mem_arr):
+# @brief
+# Check the prefilled array to ensure that there is some data to be written
+# return True or False
+# @param[in] start_addr - The adress from which to start checking
+# @param[in] mem_arr - The prefilled array of SAM data, taken from the Intel hex file
 def detect_data_in_page(start_addr,mem_arr):
     ret_val = False;
     for a in (start_addr,start_addr + SAM4C_PAGE_SIZE):
@@ -151,7 +202,18 @@ def detect_data_in_page(start_addr,mem_arr):
     return ret_val
 ###########################################################
 
-###########################################################
+
+
+##
+# @fn create_erase_section(file_ref,start_address,m_arr):
+# @brief
+# Creates a string which is written to the target file. 
+# The in is the start address of the SAM memory and the end address
+# and written to the outout file
+# This string is used by the SAM to work out which pages of memory to erase
+# @param[in] file_ref - the new file to write to
+# @param[in] start_address - The first address in the SAM to write to
+# @param[in] m_arr - the prefilled array of memory created from  the Intel hex file
 def create_erase_section(file_ref,start_address,m_arr):
     first_line = True
    # print("Create Erase Section")
@@ -170,59 +232,100 @@ def create_erase_section(file_ref,start_address,m_arr):
                 
     file_ref.write("$ " + format(lowest_address,'x').zfill(7) + " " + format(highest_address,'x').zfill(7) + "\r\n")
         
-    
-###########################################################    
 
-# Start of program
+##
+# @fn check_line_crc(line)
+# @brief
+# Checks a line of the Intel hex file to make sre its CRC is correct
+# @return True iof CRC matches, false if not
+# @param[in] line - a line of data read from the Intel hex file
+def check_line_crc(line):
+    slen = len(line)
+    index = 1
+    calculated_crc = 0    
+    while index < (slen - 4):
+        temp_str = line[index:index + 2]
+        lv = int(temp_str, base=16)
+        calculated_crc += lv
+        calculated_crc &= 0xFF
+        index += 2        
+    temp_str = line[index:index + 2]
+    read_crc = int(temp_str, base=16)
+    calculated_crc = ~calculated_crc & 0xFF
+    calculated_crc += 1
+    calculated_crc &= 0xFF
+        
+    if calculated_crc == read_crc:
+        ret_val = True
+    else:
+        ret_val = False
+        print("Calculated CRC = " + hex(calculated_crc) + " Read CRC " + hex(read_crc))
+
+    return ret_val
+
+                                   
+##
+# @fn "Main" function of file
+# @brief
+# Reads in the file specified by sys.argv[1] and reads it, 
+# a line at a time.
+n = len(sys.argv)
+if n != 3:
+    if n == 2:
+        if sys.argv[1] != "-h" and sys.argv[1] != "-help":
+            print("Invalid arguments. Input -h or ==help for help");
+    print("Input the name of the file created by WICED")
+    print("and the version number of the firmware in format x.y.z")
+    print("eg. snap40_shield-BCM920736TAG_Q32-rom-ram-Wiced-release.ota.bin 1.2.66")  
+    sys.exit()
 
 memory_array = []
-for y in range(0, MEMORY_ARRAY_SIZE):
-    memory_array.append(0xFFFF)
+for y in range(0, MEMORY_ARRAY_SIZE): # Prefill an  array the size of the SAM's memory 
+    memory_array.append(0xFFFF)       # With 0xFFFF. When file is read it will put values of
+                                      # length 8 bits into the array
     
 f = open(sys.argv[1])
 line_array = []
 offset = BASE_ADDRESS
 for l in range(0,16):
     line_array.append(0xFFFF)
-# use readline() to read the first line 
+# readline() gets the first line
 line = f.readline()
-# use the read line to read further.
-# If the file is not empty keep reading one line
-# at a time, till the file is empty
-while line:
-# in python 3 print is a builtin function, so
-    for l in range(0,16):
+# There is another readline at the end of this while statement 
+# Keep going until there are no more lines to read
+while line: 
+    for l in range(0,16):   
         line_array[l] = 0xFFFF;
     if line[0] == ':':        
-        tempStr = line[1:3]                   # Next is the number of data bytes
+        tempStr = line[1:3]                     # Next is the number of data bytes in the line
         bytes_in_line = int(tempStr, base=16)
-        tempStr = line[7:9];                   
-        data_type = int(tempStr, base=16) 
-        if data_type == 0:
-            tempStr = line[3:7]
-            address = int(tempStr, base=16) 
-            #print("Address is ", hex(address))
+        tempStr = line[7:9];                    # Then the datatype 
+        data_type = int(tempStr, base=16)       # Each line in an Intel hex file is a specific type
+        if data_type == 0:                      # Type 0 is data
+            tempStr = line[3:7]                 # This is the address within the page
+            address = int(tempStr, base=16)             
             data_string = ""
-            for l in range(0,bytes_in_line):
+            for l in range(0,bytes_in_line):    # Read in bytes as ASCII and turn into binary
                 tempStr = line[(9+(l*2)):(9+(l*2)) + 2] 
-                line_array[l] = int(tempStr, base=16) 
+                line_array[l] = int(tempStr, base=16) # Store into an array of the whole memory area
             tempStr = line[(9+(bytes_in_line*2)):(9+(bytes_in_line*2)) + 2]
-            crc = int(tempStr, base=16)
-            #print("CRC is ", hex(crc))   
-            for l in range(0,bytes_in_line):                
+            crc = int(tempStr, base=16)         # The last byte is the CRC of the data in the line            
+            for l in range(0,bytes_in_line):    # Now store the read data in the large array of SAM's whole memory
                 if line_array[l] < 0xFFFF:
                     addressToProgram = (offset-BASE_ADDRESS);
                     addressToProgram += (address + l)
                     memory_array[addressToProgram] = line_array[l]
-        elif data_type == 4:
-            tempStr = line[9:13]               # Get the offset address
+        elif data_type == 4:                    # Te offset address (ie upper 16 butes of the address to program)
+            tempStr = line[9:13]               
             offset = ((int(tempStr, base=16) << 16) & 0xFFFF0000)
             print("Offset address is ", hex(offset))
-                #data_string += tempStr + " "
-            #print (data_string)
-        elif data_type == 1:
+        elif data_type == 1:                    # End of file, last address
             end_address = offset + address + l + 1
             print("End address is ", hex(end_address))        
+    if not check_line_crc(line):                # Ensure the 
+        f.close()
+        print("Invalid crc read from file")
+        sys.exit()
     line = f.readline()    
 f.close()
 new_filename = sys.argv[1] + ".txt"
